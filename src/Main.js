506c9ecb1ab11e55a8db6b428404cc6d22665b24
Rizchel Wood <rizchelwood@gmail.com>
2019-06-20 16:47:56 -0600

    Pull in extension package and update code

--- src/Main.js
+++ src/Main.js
@@ -1,65 +1,14 @@
 import React from "react";
-import PropTypes from 'prop-types';
-
-import './style.css';
-import Gatsby from "./Gatsby";
+import PropTypes from "prop-types";
+import { ExtensionUI } from "@gatsby-cloud-pkg/gatsby-cms-extension-base"
 
 const Main = ({ config, entryData }) => {
   const { instanceUrl, authToken } = config;
   const { url } = entryData;
 
   return (
-    <div className="container">
-        {instanceUrl
-          ? (
-              <button
-                type="button"
-                className="preview-button"
-                onClick={ async () => {
-                  const normalize = part => part.replace(/\/$/, "");
-
-                  const entryUrl = url.startsWith("/") ? url.slice(1) : url;
-                  const normalizedInstanceUrl = normalize(instanceUrl);
-                  
-                  const pathQuery = `query getQualifiedSlug($entryUrl:String) { sitePage( path:{ ${entryUrl ? `regex` : `eq`}:$entryUrl } ) { path } }`;
-                  const pathVariables = entryUrl ? `/${entryUrl}\/?$/` : "/";
-                  try {
-                    const res = await fetch(`${normalizedInstanceUrl}/___graphql`, {
-                      method: "POST",
-                      headers: {
-                        "Content-Type": "application/json",
-                        "x-preview-auth-token": authToken,
-                      },
-                      body: JSON.stringify({
-                        query: pathQuery,
-                        variables: {
-                          entryUrl: pathVariables,
-                        }
-                      })
-                    });
-                    const { data } = await res.json();
-                    const path = data ? data.sitePage.path : "";
-              
-                    window.open(`${normalizedInstanceUrl}${path}`);
-                  } catch (e) {
-                    console.error(e);
-
-                    window.open(`${normalizedInstanceUrl}/${entryUrl}`);
-                  }
-                }
-              }
-              >
-                Open Preview
-              </button>
-          )
-          : (
-            <p>Missing required Gatsby Cloud instance url. Please check your extension config parameters.</p>
-          )
-        }
-      <div className="powered-by">
-        <p>Powered by:</p>
-        <Gatsby />
-      </div>
+    <div className="container" style={{ padding: '15px' }}>
+        <ExtensionUI contentSlug={url} previewInstanceUrl={instanceUrl} authToken={authToken} />
     </div>
   )
 }
