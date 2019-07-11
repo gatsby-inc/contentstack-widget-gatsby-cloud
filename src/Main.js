330808b939777fb0b30c5e142d5c2a5d818005d4
Rizchel Wood <rizchelwood@gmail.com>
2019-07-11 10:31:18 -0600

    Pass url to package only if it's a string

--- src/Main.js
+++ src/Main.js
@@ -5,10 +5,12 @@ import { ExtensionUI } from "@gatsby-cloud-pkg/gatsby-cms-extension-base"
 const Main = ({ config, entryData }) => {
   const { instanceUrl, authToken } = config;
   const { url } = entryData;
-
+  let entryUrl;
+  if (typeof url === `string`)  entryUrl = url
+  
   return (
     <div className="container" style={{ padding: '15px' }}>
-        <ExtensionUI contentSlug={url} previewInstanceUrl={instanceUrl} authToken={authToken} />
+        <ExtensionUI contentSlug={entryUrl} previewInstanceUrl={instanceUrl} authToken={authToken} />
     </div>
   )
 }
